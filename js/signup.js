a275ac1a97dafeac90d1fa2cc1cdd9c0bfe60584
diyahanggraeny <62924671+diyahanggraeny@users.noreply.github.com>
2020-05-24 01:24:35 +0700

    Add files via upload

--- js/signup.js
+++ js/signup.js
@@ -6,16 +6,56 @@ function onSignIn(googleUser) {
     document.getElementById("myname").value = profile.getName();
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+}
 
- function renderButton() {
-      gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 280,
-        'height': 40,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-      });
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+}
+
+function checkpass(){
+    var pass = (document.getElementById("mypass").value);
+    var cpass = (document.getElementById("mycpass").value);
+
+    if (pass != cpass){
+        document.getElementById('mypass').value = '';
+        document.getElementById('mycpass').value = '';
     }
+}
+
+function checknum1(){
+    var tp = (document.getElementById("mynum").value);
+    var patt = /[+]/;
+    var res = patt.test(tp);
+    var patt2 = /[0-9]/;
+    var res2 = patt2.test(tp);
+    var patt3 = /[-]/;
+    var res3 = patt3.test(tp);
+
+    if(res == false || res2 == false || res3 == false){
+        document.getElementById('mynum').value = ''; 
+    }
+
+}
+
+function checknum2(){
+    var tp = (document.getElementById("mynum2").value);
+    var patt = /[+]/;
+    var res = patt.test(tp);
+    var patt2 = /[0-9]/;
+    var res2 = patt2.test(tp);
+    var patt3 = /[-]/;
+    var res3 = patt3.test(tp);
+
+    if(res == false || res2 == false || res3 == false){
+        document.getElementById('mynum2').value = ''; 
+    }
+
+}
