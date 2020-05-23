0bec44e2774be2214293f43e42b00d031e3f2781
diyahanggraeny <62924671+diyahanggraeny@users.noreply.github.com>
2020-05-23 16:20:21 +0700

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -5,3 +5,15 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
+
+ function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 280,
+        'height': 40,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
